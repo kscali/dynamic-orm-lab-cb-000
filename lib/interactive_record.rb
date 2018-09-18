8847bf278fadf3f678b18c388ce511e3aaf388a7
Koy Saeteurn <kosatun@aol.com>
2018-09-18 21:48:29 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,7 +3,35 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end 
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true 
+    sql = "pragma table_info('#{table_name}')"
+    
+    table_info = DB[:conn].excute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end 
+    column_names.compact
+  end   
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end 
+  end 
+  
+  def save 
+    sql = "INSERT INTO 
+    #{table_name_for_insert}
+    #{col_names_for_insert}
+    VALUES(#{values_for_insert})"
+    DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end 
 
 
 
